8a82e5ffe39c80595236c4308219f495cb7aa272
Joaquin Bonvechi <100308852+JoacoBonvechi@users.noreply.github.com>
2025-02-25 20:07:06 -0300

    links conectados, vistas terminadas empezando animaciones

--- src/app/components/areas/areas.component.ts
+++ src/app/components/areas/areas.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-areas',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './areas.component.html',
   styleUrl: './areas.component.css'
 })
