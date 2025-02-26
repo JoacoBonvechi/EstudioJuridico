6dcce54ac116eabc50550070f94cddb70cf3bb4f
Joaquin Bonvechi <100308852+JoacoBonvechi@users.noreply.github.com>
2025-02-25 22:36:02 -0300

    boton

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { WhatsappButtonComponent } from "../whatsapp-button/whatsapp-button.component";
 import { AreasComponent } from "../areas/areas.component";
 import { OpinionesComponent } from "../opiniones/opiniones.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [WhatsappButtonComponent, AreasComponent, OpinionesComponent],
+  imports: [WhatsappButtonComponent, AreasComponent, OpinionesComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
