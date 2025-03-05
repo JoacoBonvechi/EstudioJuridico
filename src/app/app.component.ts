1e3a3f13a78febcd26f34a03d3080d41e14bec74
Joaquin Bonvechi <100308852+JoacoBonvechi@users.noreply.github.com>
2025-03-05 18:03:13 -0300

    se cambio opiniones, terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from "./components/footer/footer.component";
 import { HomeComponent } from './components/home/home.component';
 import { AreasComponent } from "./components/areas/areas.component";
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'EstudioJuridico';
+  private routerSubscription: Subscription;
+
+  constructor(private router: Router) {
+    this.routerSubscription = this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (typeof window !== 'undefined') {  // 👈 Evita el error en SSR
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.routerSubscription.unsubscribe(); // Evita problemas de memoria
+  }
 }
+
+
+
